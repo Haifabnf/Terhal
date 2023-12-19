0407e7c7e56793902faaa7fd4f4490c936b394c7
Haifa <haifabnf@gmail.com>
2023-12-19 15:22:12 +0300

    Initial Commit

---
+++ Terhal/TerhalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerhalApp.swift
+//  Terhal
+//
+//  Created by haifa alfoiz on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TerhalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
