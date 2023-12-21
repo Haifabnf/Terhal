f862b180ce053864dc5793b0a4eb329cc6c3fc11
Haifa <haifabnf@gmail.com>
2023-12-21 16:40:00 +0300

    main screen ui

---
+++ MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  Terhal
+//
+//  Created by haifa alfoiz on 20/12/2023.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreen()
+}
