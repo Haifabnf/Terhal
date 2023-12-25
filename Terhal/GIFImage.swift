15bbc02a817d64b9b2921002d3376ad31720809b
Taif Almezani <Taif.apple.dev@gmail.com>
2023-12-25 22:58:47 +0300

    splash page

---
+++ Terhal/GIFImage.swift
@@ -0,0 +1,18 @@
+//
+//  GIFImage.swift
+//  Terhal
+//
+//  Created by Taif on 11/06/1445 AH.
+//
+
+import SwiftUI
+
+struct GIFImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GIFImage()
+}
---
+++ Terhal/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  Splash.swift
+//  Terhal
+//
+//  Created by Taif on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splash()
+}
