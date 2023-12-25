f4414be0b5f976f57b8932263c2452c35fdc5d36
Haifa <haifabnf@gmail.com>
2023-12-25 23:29:32 +0300

    splashscreen

--- Terhal/SplashView.swift
+++ Terhal/SplashView.swift
@@ -2,25 +2,32 @@ import SwiftUI
 
 struct SplashView: View {
     @State private var imageOpacity: Double = 1.0
-    
+    @State var isActive: Bool = false
+
     var body: some View {
-        ZStack {
-            // Image as the background
-            Image("terh")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
-                .opacity(imageOpacity)
-                .animation(.easeInOut(duration: 1.5)) // Adjust the duration as needed
-            
-            
-            
-        }
-        .onAppear {
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                withAnimation {
-                    self.imageOpacity = 0.0
+        if self.isActive{
+            ContentView()
+        }else{
+            ZStack {
+                // Image as the background
+                Image("terh")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                    .opacity(imageOpacity)
+                    .animation(.easeInOut(duration: 1.5)) // Adjust the duration as needed
+                
+                
+                
+            }
+            .onAppear {
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    withAnimation {
+                        //self.imageOpacity = 0.0
+                        self.isActive = true
+                        
+                    }
                 }
             }
         }
