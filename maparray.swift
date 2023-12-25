5beb4b70f727854aab2a2c329bb46c4b1d3e74c0
felwah <felwahruma@gmail.com>
2023-12-26 00:24:05 +0300

    new

---
+++ maparray.swift
@@ -0,0 +1,18 @@
+//
+//  maparray.swift
+//  Terhal
+//
+//  Created by Felwah on 11/06/1445 AH.
+//
+
+import SwiftUI
+
+struct maparray: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    maparray()
+}
