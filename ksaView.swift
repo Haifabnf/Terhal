b236fedca5dfc81418150fcd913720485e056a2c
Haifa <haifabnf@gmail.com>
2023-12-26 11:43:44 +0300

    mm

---
+++ ksaView.swift
@@ -0,0 +1,18 @@
+//
+//  ksaView.swift
+//  Terhal
+//
+//  Created by haifa alfoiz on 26/12/2023.
+//
+
+import SwiftUI
+
+struct ksaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ksaView()
+}
---
+++ mapView.swift
@@ -0,0 +1,18 @@
+//
+//  mapView.swift
+//  Terhal
+//
+//  Created by haifa alfoiz on 26/12/2023.
+//
+
+import SwiftUI
+
+struct mapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapView()
+}
